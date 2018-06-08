7e9f8a06a3233b514cc5e2c742599207d74d380e
jipp <wolfgang.keller@wobilix.de>
2018-06-09 00:07:13 +0200

    bugfix

--- src/config-example.h
+++ src/config-example.h
@@ -12,7 +12,7 @@ const char* password_2 = "<wifi-key>";
 
 const char* mqtt_server = "<mqtt-broker>";
 const char* mqtt_port = "<port>";
-const char* mqtt_user = "<mqtt-user>";
+const char* mqtt_username = "<mqtt-user>";
 const char* mqtt_password = "<mqtt-password>";
 const char* mqtt_topic_prefix = "<mqtt-topic-prefix>";
 
