b9ec42401f14e80d5b22ae89f90ad1ad1af3adb7
jipp <wolfgang.keller@wobilix.de>
2018-06-10 12:24:41 +0200

    added sht3x sensor lib

---
+++ lib/Sensors/SHT3X.cpp
@@ -0,0 +1,37 @@
+#include "SHT3X.h"
+
+SHT3X::SHT3X(byte addr) {
+  SHT3X_I2CADDR = addr;
+}
+
+void SHT3X::begin() {
+}
+
+bool SHT3X::isAvailable() {
+  bool isAvailable = false;
+
+  Wire.beginTransmission(SHT3X_I2CADDR);
+  isAvailable = Wire.endTransmission();
+
+  return isAvailable == 0 ? true : false;
+}
+
+void SHT3X::getValue() {
+  unsigned int data[6];
+
+  Wire.beginTransmission(SHT3X_I2CADDR);
+  Wire.write(SHT31_MEAS_HIGHREP >> 8);
+  Wire.write(SHT31_MEAS_HIGHREP & 0xFF);
+  Wire.endTransmission();
+  delay(500);
+
+  Wire.requestFrom(SHT3X_I2CADDR, 6);
+  if (Wire.available() == 6) {
+    for (int i=0;i<6;i++) {
+  		data[i]=Wire.read();
+  	};
+  }
+
+ 	temperature = ((((data[0] * 256.0) + data[1]) * 175) / 65535.0) - 45;
+	humidity = ((((data[3] * 256.0) + data[4]) * 100) / 65535.0);
+}
---
+++ lib/Sensors/SHT3X.h
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+
+#include <Streaming.h>
+#include <Wire.h>
+
+class SHT3X {
+private:
+  int SHT3X_I2CADDR;
+  enum Mode {
+    SHT31_MEAS_HIGHREP_STRETCH = 0x2C06,
+    SHT31_MEAS_MEDREP_STRETCH = 0x2C0D,
+    SHT31_MEAS_LOWREP_STRETCH = 0x2C10,
+    SHT31_MEAS_HIGHREP = 0x2400,
+    SHT31_MEAS_MEDREP = 0x240B,
+    SHT31_MEAS_LOWREP = 0x2416,
+    SHT31_READSTATUS = 0xF32D,
+    SHT31_CLEARSTATUS = 0x3041,
+    SHT31_SOFTRESET = 0x30A2,
+    SHT31_HEATEREN = 0x306D,
+    SHT31_HEATERDIS = 0x3066,
+};
+
+public:
+  SHT3X(byte addr = 0x44);
+  void begin();
+  bool isAvailable();
+  void getValue();
+  float temperature=0.0;
+  float humidity=0.0;
+};
